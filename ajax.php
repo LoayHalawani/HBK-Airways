003ff2e11e6c1f904694cbddfb73ec6ad3ce726c
Loay Halawani <loayhalawani0@gmail.com>
2024-09-13 10:03:10 +0300

    Reorganize files

--- ajax.php
+++
@@ -1,9 +0,0 @@
-<?php
-require 'vendor/autoload.php';
-
-$client = new MongoDB\Client("mongodb://localhost:27017");
-$collection = $client->airline->flight;
-$ID = $_GET["id"];
-$flight = $collection->findOne(['id' => $ID]);
-echo json_encode($flight);
-?>
